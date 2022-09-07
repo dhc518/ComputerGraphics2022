cb262b93a727f345a925e84c347c0c72c0e50f89
dhc518 <58282810+dhc518@users.noreply.github.com>
2022-09-07 17:16:34 +0900

    Update CG_test1.c

--- test01/test_01/CG_test1.c
+++ test01/test_01/CG_test1.c
@@ -20,7 +20,7 @@ void rand_input(){
 
 void print_matrix()
 {
-	printf("matrix A:\t\tmatrix B:\n");
+	printf("matrix A:\tmatrix B:\n");
 	for (i = 0; i < 3; i++) {
 		for (j = 0; j < 3; j++) {
 			printf("%d", A[i][j]);
@@ -29,18 +29,19 @@ void print_matrix()
 		for (j = 0; j < 3; j++) {
 			printf("%d", B[i][j]);
 		}
+		printf("\n");
 	}
 }
 void print_explain()
 {
-	printf("m: 행렬의 곱셈\n\
-	a : 행렬의 덧셈\n\
-	d : 행렬의 뺄셈(행렬A-행렬B)\n\
-	r : 행렬식의 값(Determinant)\n\
-	t : 전치 행렬(Transposed matrix)과 그 행렬식의 값  입력한 2개의 행렬에 모두 적용한다.\n\
-	h : 3X3 행렬을 4X4 행렬로 변환하고 행렬식의 값(4행4열 행렬식 값) 출력\n\
-	s : 행렬의 값을 새로 랜덤하게 설정한다.\n\
-	q : 프로그램 종료\n");
+	printf("\m: 행렬의 곱셈\n\
+a : 행렬의 덧셈\n\
+d : 행렬의 뺄셈(행렬A-행렬B)\n\
+r : 행렬식의 값(Determinant)\n\
+t : 전치 행렬(Transposed matrix)과 그 행렬식의 값  입력한 2개의 행렬에 모두 적용한다.\n\
+h : 3X3 행렬을 4X4 행렬로 변환하고 행렬식의 값(4행4열 행렬식 값) 출력\n\
+s : 행렬의 값을 새로 랜덤하게 설정한다.\n\
+q : 프로그램 종료\n");
 
 }
 
@@ -53,7 +54,7 @@ void matrix_add()
 		printf("\n");
 	}
 }
-void matrix_sub(int a, int b)
+void matrix_sub()
 {
 	for (i = 0; i < 3; i++) {
 		for (j = 0; j < 3; j++) {
@@ -63,9 +64,14 @@ void matrix_sub(int a, int b)
 	}
 
 }
-void matrix_mul(int a, int b)
+void matrix_mul()
 {
-	
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
+			printf("%d ", A[i][0]*B[0][j]+ A[i][1] * B[1][j] + A[i][2] * B[2][j]);
+		}
+		printf("\n");
+	}
 }
 void matrix_determinant(int a, int b)
 {
@@ -92,7 +98,7 @@ void matrix_33to44(int a, int b)
 int main()
 {
 	
-	char command[2];
+	char command[1];
 
 	rand_input(A, B);
 	
@@ -109,6 +115,8 @@ int main()
 		else if (command == 't') { matrix_transpose(A, B); }
 		else if (command == 'h') { matrix_33to44(A, B); }
 		else if (command == 's') { rand_input(A, B); }
+		print_explain();
+		scanf_s("%c", &command);
 	}
 
 	return 0;
